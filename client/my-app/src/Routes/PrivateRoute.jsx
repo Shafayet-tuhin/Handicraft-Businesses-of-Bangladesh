b037e1b0ce65e8ecc7ecaa7446a165a94747e3ad
Shafayet-tuhin <shafayettuhin00@gmail.com>
2024-08-19 13:17:41 +0600

    included jwt decoded

--- client/my-app/src/Routes/PrivateRoute.jsx
+++ client/my-app/src/Routes/PrivateRoute.jsx
@@ -1,20 +1,45 @@
 import React, { useContext } from 'react'
 import { AuthContext } from '../Context/AuthProvider'
 import { Navigate } from 'react-router-dom'
+import { jwtDecode } from "jwt-decode";
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
 
-    const {user , loading} = useContext(AuthContext) 
+    const { user, loading , logOut} = useContext(AuthContext)
 
-    if ( loading ) {
+
+
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        return <Navigate to='/login' replace></Navigate>
+    }
+
+    const decoded = jwtDecode(token);
+
+    // console.log(decoded);
+
+    const decodedHeader = jwtDecode(token, { header: true });
+    // console.log(decodedHeader);
+
+
+
+    if (loading) {
         return <progress className="progress w-56"></progress>
     }
+    // console.log(Date.now())
+    // console.log(decoded.exp ,"===>", Date.now() / 1000)
 
-    if ( user?.email) {
-        return children
+    if (decoded.exp < Date.now() / 1000) {
+        logOut();
+        return <Navigate to='/login' replace></Navigate>
     }
 
-    return <Navigate to='/login' replace ></Navigate>
+ 
+     return children
+    
+
+    
 }
 
 export default PrivateRoute
